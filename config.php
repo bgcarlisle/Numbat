b68602339b55e3fb12a454d6c383c3b38d2aa234
Benjamin Carlisle <murph@bgcarlisle.com>
2014-01-09 17:22:58 -0500

    Changed config so that it works on my dev computer

--- config.php
+++ config.php
@@ -1,10 +1,12 @@
 <?php
 
-define('DB_USER', '');
-define('DB_PASS', '');
-define('DB_NAME', '');
-define('DB_HOST', '');
-define('ABS_PATH', '');
-define('SITE_URL', '');
+define('DB_USER', 'numbat_db_user');
+define('DB_PASS', 'dahlia6{ichthyologist');
+define('DB_NAME', 'numbat_db');
+define('DB_HOST', '127.0.0.1');
+define('ABS_PATH', '/Users/carlisle/Sites/Numbat/');
+define('SITE_URL', 'http://127.0.0.1/~carlisle/Numbat/');
+
+include_once (ABS_PATH . "functions.php");
 
 ?>
