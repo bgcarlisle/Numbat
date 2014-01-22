d77333ccbe0f076b1f05397b01b2de931208ebe9
Benjamin Carlisle <murph@bgcarlisle.com>
2014-01-22 18:58:34 -0500

    Aesthetic changes

--- header.php
+++ header.php
@@ -5,22 +5,6 @@
 
 	<title>Numbat</title>
 	
-	<!-- Google Analytics -->
-	<script type="text/javascript">
-
-	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', 'UA-8721708-1']);
-	  _gaq.push(['_trackPageview']);
-	
-	  (function() {
-	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
-	
-	</script>
-	<!-- / Google Analytics -->
-	
 	<!-- jQuery -->
 	<script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.js">
 	</script>
@@ -41,6 +25,8 @@
 	<link rel="stylesheet" href="<?php echo SITE_URL; ?>css/reset.css" />
 	<link rel="stylesheet" href="<?php echo SITE_URL; ?>css/numbat.css" />
 	<!-- / CSS -->
+	
+	<link rel="SHORTCUT ICON" href="<?php echo SITE_URL; ?>images/favicon.ico"/>
 
 </head>
 
@@ -73,8 +59,7 @@
 
 if ( nbt_user_is_logged_in () ) {
 	
-	?>
-	<nav>
+	?><nav>
 		<ul>
 			<li>
 				<a href="<?php echo SITE_URL; ?>users/">
@@ -119,8 +104,7 @@ if ( nbt_user_is_logged_in () ) {
 				</a>
 			</li>
 		</ul>
-	</nav>
-	<?php
+	</nav><?php
 	
 }
 	
