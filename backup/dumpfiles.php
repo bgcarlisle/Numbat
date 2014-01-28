1081865451267204e8402335a28a9e852035de82
Benjamin Carlisle <murph@bgcarlisle.com>
2014-01-28 12:20:34 -0500

    Different users can do different things with backups

--- backup/dumpfiles.php
+++ backup/dumpfiles.php
@@ -2,16 +2,48 @@
 
 $files = scandir ( ABS_PATH . "backup/dumpfiles/");
 
-?><ul><?php
+?><table class="nbtTabledData">
+<tr class="nbtTableHeaders">
+	<td>Backup file</td>
+	<?php
+	
+	if ( nbt_get_privileges_for_userid ( $_SESSION['nbt_userid'] ) == 4 ) {
+		
+		?><td>Restore from backup</td><?php
+		
+	}
+	
+	?>
+</tr>
+<?php
 
 foreach ( $files as $file ) {
 	
 	if ( substr ($file, 0, 1) != "." ) {
 		
-		?><li><a href="<?php echo SITE_URL; ?>backup/dumpfiles/<?php echo $file; ?>"><?php echo $file; ?></a></li><?php
+		switch ( nbt_get_privileges_for_userid ( $_SESSION['nbt_userid'] ) ) {
+			
+			case 4:
+				
+				?><tr>
+					<td><a href="<?php echo SITE_URL; ?>backup/dumpfiles/<?php echo $file; ?>"><?php echo $file; ?></a></td>
+					<td><button>Restore</button></td>
+				</tr><?php
+				
+			break;
+			
+			case 2:
+				
+				?><tr>
+					<td><?php echo $file; ?></td>
+				</tr><?php
+				
+			break;
+			
+		}
 		
 	}
 	
 }
 
-?></ul>
+?></table>
