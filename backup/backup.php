90607204ff67c98cbd152812e2f43ad9bc459ec3
Benjamin Carlisle <murph@bgcarlisle.com>
2014-09-12 16:55:50 -0400

    Aesthetic changes to the backup indicator

--- backup/backup.php
+++ backup/backup.php
@@ -11,4 +11,6 @@
 
 </div>
 <div id="nbtCoverup" style="display: none; background: #ccc; opacity: 0.5; z-index: 1; width: 100%; height: 100%; position: fixed; top: 0; left: 0;">&nbsp;</div>
-<div id="nbtThinky" style="display: none; z-index: 2; position: fixed; top: 100px; width: 100%; text-align: center;">Backin' up ...</div>
+<div id="nbtThinky" style="display: none; z-index: 2; position: fixed; top: 100px; width: 100%; text-align: center;">
+	<div style="padding: 10px 20px 10px 20px; border: 2px solid #666; border-radius: 5px; background: #eee; color: #666; display: inline;">Backin' up ...</div>
+</div>
