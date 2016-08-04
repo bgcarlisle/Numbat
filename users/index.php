6a9a830e4298783c956a959c4f9dab4fea5b9522
bgcarlisle <murph@bgcarlisle.com>
2016-08-04 13:33:01 -0400

    Bug fix on users admin page

--- users/index.php
+++ users/index.php
@@ -3,28 +3,30 @@
 include_once ('../config.php');
 
 if ( nbt_user_is_logged_in () ) { // User is logged in
-	
+
 	if ( nbt_get_privileges_for_userid ( $_SESSION[INSTALL_HASH . '_nbt_userid'] ) == 4 ) {
-		
+
 		include ( ABS_PATH . "header.php" );
 		include ( ABS_PATH . "users/users.php" );
-		
+
 	} else {
-		
+
 		$nbtErrorText = "You do not have permission to administer users.";
-		
+
 		include ( ABS_PATH . "header.php" );
 		include ( ABS_PATH . "error.php" );
-		
+
 	}
-	
+
 } else {
-	
+
 	$nbtErrorText = "You are not logged in.";
-			
+
 	include ( ABS_PATH . "header.php" );
 	include ( ABS_PATH . "error.php" );
-	
+
 }
+
+include ( ABS_PATH . "footer.php" );
 
 ?>
