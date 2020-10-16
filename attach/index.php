3de673b37008f1ad9f52cb61030a53d8d18b2939
Benjamin G Carlisle <bgcarlisle@users.noreply.github.com>
2020-10-16 16:16:08 +0200

    Fixed white space

--- attach/index.php
+++ attach/index.php
@@ -4,52 +4,52 @@ include_once ('../config.php');
 
 if ( nbt_user_is_logged_in () ) { // User is logged in
 
-      if ( nbt_get_privileges_for_userid ( $_SESSION[INSTALL_HASH . '_nbt_userid'] ) == 4 ) {
+    if ( nbt_get_privileges_for_userid ( $_SESSION[INSTALL_HASH . '_nbt_userid'] ) == 4 ) {
 
-            if ( isset ( $_GET['action'] ) ) {
+        if ( isset ( $_GET['action'] ) ) {
 
-                  switch ( $_GET['action'] ) {
+            switch ( $_GET['action'] ) {
 
-                        case "viewrefset":
+                case "viewrefset":
 
-                              include ( ABS_PATH . "header.php" );
-                              include ( ABS_PATH . "attach/refset.php" );
+                    include ( ABS_PATH . "header.php" );
+                    include ( ABS_PATH . "attach/refset.php" );
 
-                        break;
+                    break;
 
-                        case "new":
+                case "new":
 
-                              include ( ABS_PATH . "header.php" );
-                              include ( ABS_PATH . "attach/new.php" );
+                    include ( ABS_PATH . "header.php" );
+                    include ( ABS_PATH . "attach/new.php" );
 
-                        break;
-
-                  }
-
-            } else {
-
-                  include ( ABS_PATH . "header.php" );
-                  include ( ABS_PATH . "attach/attachments.php" );
+                    break;
 
             }
 
-
-
-      } else {
-
-            $nbtErrorText = "You do not have permission to attach files.";
+        } else {
 
             include ( ABS_PATH . "header.php" );
-            include ( ABS_PATH . "error.php" );
+            include ( ABS_PATH . "attach/attachments.php" );
 
-      }
+        }
+
+
+
+    } else {
+
+        $nbtErrorText = "You do not have permission to attach files.";
+
+        include ( ABS_PATH . "header.php" );
+        include ( ABS_PATH . "error.php" );
+
+    }
 
 } else {
 
-      $nbtErrorText = "You are not logged in.";
+    $nbtErrorText = "You are not logged in.";
 
-      include ( ABS_PATH . "header.php" );
-      include ( ABS_PATH . "error.php" );
+    include ( ABS_PATH . "header.php" );
+    include ( ABS_PATH . "error.php" );
 
 }
 
