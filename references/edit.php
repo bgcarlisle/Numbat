982899f80499d51b049fc26069140b4c786e4326
Benjamin G Carlisle <bgcarlisle@users.noreply.github.com>
2020-10-16 14:43:21 +0200

    White space fix

--- references/edit.php
+++ references/edit.php
@@ -3,8 +3,8 @@
 $refset = nbt_get_refset_for_id ( $_GET['refset'] );
 
 ?><div class="nbtContentPanel nbtGreyGradient">
-	<h2>Edit reference set</h2>
-	<p>Name</p>
-	<input id="nbtNewRefSetName" type="text" value="<?php echo $refset['name']; ?>" onblur="nbtChangeRefSetName(<?php echo $refset['id']; ?>);">
-	<span class="nbtFinePrint nbtHidden nbtFeedback" id="nbtNewRefSetNameFeedback">&nbsp;</span>
+    <h2>Edit reference set</h2>
+    <p>Name</p>
+    <input id="nbtNewRefSetName" type="text" value="<?php echo $refset['name']; ?>" onblur="nbtChangeRefSetName(<?php echo $refset['id']; ?>);">
+    <span class="nbtFinePrint nbtHidden nbtFeedback" id="nbtNewRefSetNameFeedback">&nbsp;</span>
 </div>
