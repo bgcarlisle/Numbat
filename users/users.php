2d67e69b1ad90d33ea0a936d124e80a021e72d24
Benjamin G Carlisle <bgcarlisle@users.noreply.github.com>
2020-10-20 11:59:52 +0200

    White space

--- users/users.php
+++ users/users.php
@@ -1,74 +1,74 @@
 <div class="nbtContentPanel nbtGreyGradient">
-	<h2>
-		<img src="<?php echo SITE_URL; ?>images/useradmin.png" class="nbtTitleImage">
-		User admininstration
-	</h2>
-	<table class="nbtTabledData">
-		<tr class="nbtTableHeaders">
-			<td>Username</td>
-			<td>Email</td>
-			<td>Privileges</td>
-		</tr>
-		<?php
+    <h2>
+	<img src="<?php echo SITE_URL; ?>images/useradmin.png" class="nbtTitleImage">
+	User admininstration
+    </h2>
+    <table class="nbtTabledData">
+	<tr class="nbtTableHeaders">
+	    <td>Username</td>
+	    <td>Email</td>
+	    <td>Privileges</td>
+	</tr>
+	<?php
 
-		$allusers = nbt_get_all_users ();
+	$allusers = nbt_get_all_users ();
 
-		foreach ( $allusers as $user ) {
+	foreach ( $allusers as $user ) {
 
-			?><tr>
-				<td><?php echo $user['username']; ?></td>
-				<td><?php echo $user['email']; ?></td>
-				<td><?php
+	?><tr>
+	    <td><?php echo $user['username']; ?></td>
+	    <td><?php echo $user['email']; ?></td>
+	    <td><?php
 
-					if ( $user['id'] == $_SESSION[INSTALL_HASH . '_nbt_userid']) {
+		if ( $user['id'] == $_SESSION[INSTALL_HASH . '_nbt_userid']) {
 
-						?>Admin<?php
+		?>Admin<?php
 
-					} else {
+		       } else {
 
-						?><select id="nbtUserPrivileges<?php echo $user['id']; ?>" onchange="nbtChangeUserPrivileges(<?php echo $user['id']; ?>);">
-							<option value="0"<?php
+		       ?><select id="nbtUserPrivileges<?php echo $user['id']; ?>" onchange="nbtChangeUserPrivileges(<?php echo $user['id']; ?>);">
+		<option value="0"<?php
 
-								if ( $user['privileges'] == 0 ) {
+				 if ( $user['privileges'] == 0 ) {
 
-									?> selected<?php
+				 ?> selected<?php
 
-								}
+					    }
 
-							?>>None</option>
-							<option value="2"<?php
+					    ?>>None</option>
+		<option value="2"<?php
 
-								if ( $user['privileges'] == 2 ) {
+				 if ( $user['privileges'] == 2 ) {
 
-									?> selected<?php
+				 ?> selected<?php
 
-								}
+					    }
 
-							?>>User</option>
-							<option value="4"<?php
+					    ?>>User</option>
+		<option value="4"<?php
 
-								if ( $user['privileges'] == 4 ) {
+				 if ( $user['privileges'] == 4 ) {
 
-									?> selected<?php
+				 ?> selected<?php
 
-								}
+					    }
 
-							?>>Admin</option>
-						</select><?php
+					    ?>>Admin</option>
+		       </select><?php
 
-					}
+				}
 
 				?></td>
-			</tr><?php
+	</tr><?php
 
-		}
+	     }
 
-		?>
-	</table>
+	     ?>
+    </table>
 
-	<div class="nbtHidden nbtFeedbackGood nbtFeedback nbtFinePrint" id="nbtPrivilegeFeedback">&nbsp;</div>
+    <div class="nbtHidden nbtFeedbackGood nbtFeedback nbtFinePrint" id="nbtPrivilegeFeedback">&nbsp;</div>
 
-	<p>Admin: can assign extractions, edit reference sets and forms, grant user privileges, do extractions and export data</p>
-	<p>User: can do extractions and reconcile extractions with other users only</p>
+    <p>Admin: can assign extractions, edit reference sets and forms, grant user privileges, do extractions and export data</p>
+    <p>User: can do extractions and reconcile extractions with other users only</p>
 
 </div>
