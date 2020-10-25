b1eabac4e72bb9d4817e10954bcbb4a6dea58819
Benjamin G Carlisle <bgcarlisle@users.noreply.github.com>
2020-10-25 12:20:38 +0100

    Removing reference to Signals

--- install/install.php
+++ install/install.php
@@ -2,18 +2,18 @@
 
 if ($_SERVER["HTTPS"] == "on") {
 
-	$numbaturl = "https://" . $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+    $numbaturl = "https://" . $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
 
 } else {
 
-	$numbaturl = "http://" . $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+    $numbaturl = "http://" . $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
 
 }
 
 ?><!DOCTYPE html>
 <html lang="en">
 
-<head>
+    <head>
 
 	<title>Numbat</title>
 
@@ -29,74 +29,74 @@ if ($_SERVER["HTTPS"] == "on") {
 	<!-- Numbat JS -->
 	<script type="text/javascript">
 
-		function nbtTestMySQLConnexion () {
+	 function nbtTestMySQLConnexion () {
 
-			$.ajax ({
-				url: '<?php echo $numbaturl; ?>install/testmysql.php',
-				type: 'post',
-				data: {
-					dbusername: $('#nbtDBusername').val(),
-					dbpassword: $('#nbtDBpassword').val(),
-					dbname: $('#nbtDBname').val(),
-					dbhost: $('#nbtDBhost').val()
-				},
-				dataType: 'html'
-			}).done ( function (html) {
+	     $.ajax ({
+		 url: '<?php echo $numbaturl; ?>install/testmysql.php',
+		 type: 'post',
+		 data: {
+		     dbusername: $('#nbtDBusername').val(),
+		     dbpassword: $('#nbtDBpassword').val(),
+		     dbname: $('#nbtDBname').val(),
+		     dbhost: $('#nbtDBhost').val()
+		 },
+		 dataType: 'html'
+	     }).done ( function (html) {
 
-				$('#nbtDBTestFeedback').html(html);
+		 $('#nbtDBTestFeedback').html(html);
 
-				if ( html == 'Successful connexion to MySQL database' ) {
+		 if ( html == 'Successful connexion to MySQL database' ) {
 
-					$('#nbtDBTestFeedback').removeClass('nbtFeedbackBad').addClass('nbtFeedbackGood');
+		     $('#nbtDBTestFeedback').removeClass('nbtFeedbackBad').addClass('nbtFeedbackGood');
 
-				} else {
+		 } else {
 
-					$('#nbtDBTestFeedback').removeClass('nbtFeedbackGood').addClass('nbtFeedbackBad');
+		     $('#nbtDBTestFeedback').removeClass('nbtFeedbackGood').addClass('nbtFeedbackBad');
 
-				}
+		 }
 
-				$('#nbtDBTestFeedback').slideDown( 500, function () {
+		 $('#nbtDBTestFeedback').slideDown( 500, function () {
 
-					setTimeout ( function () {
+		     setTimeout ( function () {
 
-						$('#nbtDBTestFeedback').slideUp(500);
+			 $('#nbtDBTestFeedback').slideUp(500);
 
-					}, 3000 );
+		     }, 3000 );
 
-				});
+		 });
 
-			});
+	     });
 
-		}
+	 }
 
-		function nbtWriteConfig () {
+	 function nbtWriteConfig () {
 
-			$.ajax ({
-				url: '<?php echo $numbaturl; ?>install/writeconfig.php',
-				type: 'post',
-				data: {
-					dbusername: $('#nbtDBusername').val(),
-					dbpassword: $('#nbtDBpassword').val(),
-					dbname: $('#nbtDBname').val(),
-					dbhost: $('#nbtDBhost').val(),
-					abs_path: $('#nbtAbsPath').val(),
-					site_url: $('#nbtSiteURL').val(),
-					nbt_projname: $('#nbtProjectName').val(),
-					nbt_username: $('#nbtUsername').val(),
-					nbt_password: $('#nbtPassword').val(),
-					nbt_email: $('#nbtEmail').val()
-				},
-				dataType: 'html'
-			}).done ( function (html) {
+	     $.ajax ({
+		 url: '<?php echo $numbaturl; ?>install/writeconfig.php',
+		 type: 'post',
+		 data: {
+		     dbusername: $('#nbtDBusername').val(),
+		     dbpassword: $('#nbtDBpassword').val(),
+		     dbname: $('#nbtDBname').val(),
+		     dbhost: $('#nbtDBhost').val(),
+		     abs_path: $('#nbtAbsPath').val(),
+		     site_url: $('#nbtSiteURL').val(),
+		     nbt_projname: $('#nbtProjectName').val(),
+		     nbt_username: $('#nbtUsername').val(),
+		     nbt_password: $('#nbtPassword').val(),
+		     nbt_email: $('#nbtEmail').val()
+		 },
+		 dataType: 'html'
+	     }).done ( function (html) {
 
-				$('#nbtInstallPane').html(html);
+		 $('#nbtInstallPane').html(html);
 
-			});
+	     });
 
-		}
+	 }
 
 	</script>
-	<!-- / Signals JS -->
+	<!-- / Numbat JS -->
 
 	<!-- CSS -->
 	<link rel="stylesheet" href="<?php echo $numbaturl; ?>css/reset.css" />
@@ -105,16 +105,16 @@ if ($_SERVER["HTTPS"] == "on") {
 
 	<link rel="SHORTCUT ICON" href="<?php echo $numbaturl; ?>images/favicon.ico"/>
 
-</head>
+    </head>
 
-<body>
+    <body>
 
-<div id="nbtTopBanner">
-	<h1>Numbat</h1>
-</div>
-<div id="nbtInstallPane" class="nbtSigninPanel nbtGreyGradient" style="margin-bottom: 40px;">
-	<h2>Install Numbat</h2>
-	<div class="nbtSubExtraction">
+	<div id="nbtTopBanner">
+	    <h1>Numbat</h1>
+	</div>
+	<div id="nbtInstallPane" class="nbtSigninPanel nbtGreyGradient" style="margin-bottom: 40px;">
+	    <h2>Install Numbat</h2>
+	    <div class="nbtSubExtraction">
 		<h3>MySQL details</h3>
 		<p class="nbtFinePrint">Caution: This installer will write over any previous installation.</p>
 		<p>Database username</p>
@@ -128,23 +128,23 @@ if ($_SERVER["HTTPS"] == "on") {
 		<input type="text" id="nbtDBhost">
 		<button style="display: block; margin-top: 25px;" onclick="nbtTestMySQLConnexion();">Test database connexion</button>
 		<p id="nbtDBTestFeedback" class="nbtFeedback nbtHidden nbtFinePrint">&nbsp;</p>
-	</div>
-	<div class="nbtSubExtraction">
+	    </div>
+	    <div class="nbtSubExtraction">
 		<h3>Site details</h3>
 		<p class="nbtFinePrint">Should auto-detect correctly; change only if you know what you're doing.</p>
 		<p>Absolute path to installation</p>
 		<p class="nbtFinePrint">Include leading and trailing slashes; e.g. "/home/webspace/numbat/"</p>
 		<input id="nbtAbsPath" type="text" value="<?php
 
-		echo substr (__DIR__, 0, strlen ( __DIR__ ) - 7);
+							  echo substr (__DIR__, 0, strlen ( __DIR__ ) - 7);
 
-		?>">
+							  ?>">
 		<p>Site URL</p>
 		<p class="nbtFinePrint">Include https:// at beginning and trailing slash; e.g. "https://www.website.com/numbat/"</p>
 		<input id="nbtSiteURL" type="text" value="<?php echo $numbaturl; ?>">
-	</div>
-	<div class="nbtSubExtraction">
-	<h3>Numbat details</h3>
+	    </div>
+	    <div class="nbtSubExtraction">
+		<h3>Numbat details</h3>
 		<p>Numbat project name</p>
 		<p class="nbtFinePrint">E.g. "Meta-analyses for Signals, Safety and Success grant"</p>
 		<input type="text" id="nbtProjectName">
@@ -156,8 +156,8 @@ if ($_SERVER["HTTPS"] == "on") {
 		<p>Admin email</p>
 		<p class="nbtFinePrint">This is the email that Numbat will use when sending confirmation emails.</p>
 		<input type="text" id="nbtEmail">
+	    </div>
+	    <button onclick="nbtWriteConfig();">Install Numbat</button>
 	</div>
-	<button onclick="nbtWriteConfig();">Install Numbat</button>
-</div>
-</body>
+    </body>
 </html>
