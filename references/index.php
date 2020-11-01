8f88a303b5a19a6b6ea372902dd014f2b455cdf4
Benjamin G Carlisle <bgcarlisle@users.noreply.github.com>
2020-11-01 13:04:20 +0100

    Fixed white space

--- references/index.php
+++ references/index.php
@@ -3,45 +3,45 @@
 include_once ('../config.php');
 
 if ( nbt_user_is_logged_in () ) { // User is logged in
+    
+    if ( nbt_get_privileges_for_userid ( $_SESSION[INSTALL_HASH . '_nbt_userid'] ) == 4 ) {
 	
-	if ( nbt_get_privileges_for_userid ( $_SESSION[INSTALL_HASH . '_nbt_userid'] ) == 4 ) {
-		
-		if ( isset ( $_GET['action'] ) ) {
-			
-			switch ( $_GET['action'] ) {
-				
-				case "edit":
-					
-					include ( ABS_PATH . "header.php" );
-					include ( ABS_PATH . "references/edit.php" );
-					
-				break;
-				
-			}
-			
-		} else {
-		
-			include ( ABS_PATH . "header.php" );
-			include ( ABS_PATH . "references/references.php" );
-		
-		}
-		
+	if ( isset ( $_GET['action'] ) ) {
+	    
+	    switch ( $_GET['action'] ) {
+		    
+		case "edit":
+		    
+		    include ( ABS_PATH . "header.php" );
+		    include ( ABS_PATH . "references/edit.php" );
+		    
+		    break;
+		    
+	    }
+	    
 	} else {
-		
-		$nbtErrorText = "You do not have permission to manage reference sets.";
-		
-		include ( ABS_PATH . "header.php" );
-		include ( ABS_PATH . "error.php" );
-		
+	    
+	    include ( ABS_PATH . "header.php" );
+	    include ( ABS_PATH . "references/references.php" );
+	    
 	}
 	
-} else {
+    } else {
 	
-	$nbtErrorText = "You are not logged in.";
-			
+	$nbtErrorText = "You do not have permission to manage reference sets.";
+	
 	include ( ABS_PATH . "header.php" );
 	include ( ABS_PATH . "error.php" );
 	
+    }
+    
+} else {
+    
+    $nbtErrorText = "You are not logged in.";
+    
+    include ( ABS_PATH . "header.php" );
+    include ( ABS_PATH . "error.php" );
+    
 }
 
 include ( ABS_PATH . "footer.php" );
