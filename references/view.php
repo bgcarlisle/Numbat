70ecc48bfa6f7f0db96640aa5f1ef4b557b200f0
Benjamin G Carlisle <bgcarlisle@users.noreply.github.com>
2021-04-12 12:46:55 +0200

    Add style data to "Add new ref" button

--- references/view.php
+++ references/view.php
@@ -7,7 +7,7 @@ $references = nbt_get_all_references_for_refset ( $_GET['refset'] );
 ?><div style="padding: 20px 20px 80px 20px;">
     <h2>Reference set: <?php echo $refset['name']; ?></h2>
     <p><?php echo count($references); ?> reference(s)</p>
-    <button onclick="nbtAddNewReferenceToRefSet(<?php echo $refset['id']; ?>)">Add a new reference</button>
+    <button onclick="nbtAddNewReferenceToRefSet(<?php echo $refset['id']; ?>)" style="margin: 5px 0 10px 0;">Add a new reference</button>
     <table class="nbtTabledData">
 	<tr class="nbtTableHeaders">
 	    <td></td>
