d4eb3fa767cd4b078d3e6571c8c3a0c46c654fa6
bgcarlisle <bgcarlisle@users.noreply.github.com>
2021-09-13 11:19:05 +0200

    Copied code from 2.12 import into 2.13 import section

--- forms/importform.php
+++ forms/importform.php
@@ -237,6 +237,168 @@ if ( nbt_get_privileges_for_userid ( $_SESSION[INSTALL_HASH . '_nbt_userid'] ) =
 			break;
 
 		    case "2.13":
+
+			
+			// Make a new extraction form with the imported metadata
+			$newformid = nbt_new_extraction_form (
+			    $import['name'],
+			    $import['description'],
+			    $import['version'],
+			    $import['author'],
+			    $import['affiliation'],
+			    $import['project'],
+			    $import['protocol'],
+			    $import['projectdate']
+			);
+
+			// Decode the JSON for all the element types that have
+			// an internal data structure
+			$elements = json_decode($import['elements'], true);
+			$selectoptions = json_decode($import['selectoptions'], true);
+			$tabledatacols = json_decode($import['tabledatacols'], true);
+			$subelements = json_decode($import['subelements'], true);
+			$citecols = json_decode($import['citationscols'], true);
+
+			// Loop through all the imported elements and re-create
+			// them in the current Numbat instance individually
+			$peid = 0;
+			foreach ( $elements as $element ) {
+
+			    // Get the highest element id in the form
+			    $peid = nbt_get_highest_eid_in_form ( $newformid );
+
+			    switch ( $element['type'] ) {
+
+				case "section_heading":
+				    nbt_add_section_heading ($newformid, $peid, $element['displayname'], $element['codebook'], $element['toggle']);
+				    break;
+
+				case "timer":
+				    nbt_add_extraction_timer($newformid, $peid, $element['codebook'], $element['toggle']);
+				    break;
+
+				case "open_text":
+				    nbt_add_open_text_field ($newformid, $peid, $element['displayname'], $element['columnname'], $element['codebook'], $element['toggle'], $element['regex']);
+				    break;
+
+				case "text_area":
+				    nbt_add_text_area_field ($newformid, $peid, $element['displayname'], $element['columnname'], $element['codebook'], $element['toggle']);
+				    break;
+
+				case "date_selector":
+				    nbt_add_date_selector ($newformid, $peid, $element['displayname'], $element['columnname'], $element['codebook'], $element['toggle']);
+				    break;
+
+				case "single_select":
+				    $newelementid = nbt_add_single_select ($newformid, $peid, $element['displayname'], $element['columnname'], $element['codebook'], $element['toggle']);
+
+				    foreach ($selectoptions as $opt) {
+					if ($element['id'] == $opt['elementid']) {
+					    nbt_add_single_select_option ($newelementid, $opt['displayname'], $opt['dbname'], $opt['toggle']);
+					}
+				    }
+				    break;
+
+				case "multi_select":
+				    $newelementid = nbt_add_multi_select ($newformid, $peid, $element['displayname'], $element['columnname'], $element['codebook'], $element['toggle']);
+
+				    foreach ($selectoptions as $opt) {
+					if ($element['id'] == $opt['elementid']) {
+					    nbt_add_multi_select_option ($newelementid, $opt['displayname'], $opt['dbname'], $opt['toggle']);
+					}
+				    }
+				    break;
+
+				case "country_selector":
+				    nbt_add_country_selector ($newformid, $peid, $element['displayname'], $element['columnname'], $element['codebook'], $element['toggle']);
+				    break;
+
+				case "table_data":
+				case "ltable_data":
+				    $newelementid = nbt_add_table_data ($newformid, $peid, $element['type'], $element['displayname'], $element['columnname'], $element['codebook'], $element['toggle']);
+
+				    foreach ($tabledatacols as $col) {
+					if ($element['id'] == $col['elementid']) {
+					    nbt_add_table_data_column ($newelementid, $element['type'], FALSE, $col['displayname'], $col['dbname']);
+					}
+				    }
+				    break;
+
+				case "citations":
+				    $newelementid = nbt_add_citation_selector ($newformid, $peid, $element['displayname'], $element['columnname'], $element['codebook'], $element['toggle']);
+
+				    foreach ($citecols as $col) {
+					if ($element['id'] == $col['elementid']) {
+					    nbt_add_citation_property ($newelementid, $col['displayname'], $col['dbname'], $col['remind'], $col['caps']);
+					}
+				    }
+				    break;
+
+				case "sub_extraction":
+				    $newelementid = nbt_add_sub_extraction ($newformid, $peid, $element['displayname'], $element['columnname'], $element['codebook'], $element['toggle']);
+
+				    // Loop through all the sub-extraction elements and add them
+				    foreach ($subelements as $sel) {
+					if ($element['id'] == $sel['elementid']) {
+					    switch ($sel['type']) {
+						case "open_text":
+						    nbt_add_sub_open_text_field ($newelementid, $sel['displayname'], $sel['dbname'], $sel['regex'], $sel['copypreviousprompt'], $sel['codebook'], $sel['toggle']);
+						    break;
+						case "date_selector":
+						    nbt_add_sub_date_selector ($newelementid, $sel['displayname'], $sel['dbname'], $sel['copypreviousprompt'], $sel['codebook'], $sel['toggle']);
+						    break;
+						case "single_select":
+						    $newseid = nbt_add_sub_single_select ($newelementid, $sel['displayname'], $sel['dbname'], $sel['copypreviousprompt'], $sel['codebook'], $sel['toggle']);
+
+						    foreach ($selectoptions as $opt) {
+							if ($sel['id'] == $opt['subelementid']) {
+							    nbt_add_sub_single_select_option ($newseid, $opt['displayname'], $opt['dbname'], $opt['toggle']);
+							}
+						    }
+						    break;
+						case "multi_select":
+						    $newseid = nbt_add_sub_multi_select ($newelementid, $sel['displayname'], $sel['dbname'], $sel['copypreviousprompt'], $sel['codebook'], $sel['toggle']);
+
+						    foreach ($selectoptions as $opt) {
+							if ($sel['id'] == $opt['subelementid']) {
+							    nbt_add_sub_multi_select_option ($newseid, $opt['displayname'], $opt['dbname'], $opt['toggle']);
+							}
+						    }
+						    break;
+						case "table_data":
+						    $newseid = nbt_add_sub_table ($newelementid, $sel['displayname'], $sel['dbname'], $sel['codebook'], $sel['toggle']);
+
+						    foreach ($tabledatacols as $col) {
+							if ($sel['id'] == $col['subelementid']) {
+							    nbt_add_table_data_column ($newseid, $sel['type'], TRUE, $col['displayname'], $col['dbname']);
+							}
+						    }
+						    break;
+						    
+					    }
+					}
+				    }
+				    break;
+
+				case "assignment_editor":
+				    nbt_add_assignment_editor ($newformid, $peid, $element['displayname'], $element['codebook'], $element['toggle']);
+				    break;
+
+				    
+				case "reference_data":
+				    nbt_add_reference_data ($newformid, $peid, $element['displayname'], $element['columnname'], $element['codebook'], $element['toggle']);
+				    break;
+
+				case "prev_select":
+				    nbt_add_prev_select ($newformid, $peid, $element['displayname'], $element['columnname'], $element['codebook'], $element['toggle']);
+				    break;
+				    
+			    }
+			    
+			}
+
+			include ( ABS_PATH . "forms/forms.php");
+
 			break;
 
 		    default:
