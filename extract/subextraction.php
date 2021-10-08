3e1f87bc370f97cf3bc2ebb11c3c7e3155346652
bgcarlisle <bgcarlisle@users.noreply.github.com>
2021-10-08 13:54:07 +0200

    Started subextraction tags element

--- extract/subextraction.php
+++ extract/subextraction.php
@@ -6,34 +6,81 @@ $subextractions = nbt_get_sub_extractions ( $nbtSubExtractionElementID, $nbtExtr
 
 foreach ( $subextractions as $subextraction ) {
 
-	?><div class="nbtSubExtraction" id="nbtSubExtractionInstance<?php echo $nbtSubExtractionElementID; ?>-<?php echo $subextraction['id']; ?>">
-		<button style="float: right;" onclick="$(this).fadeOut(0);$('#nbtSubRemoveExtraction<?php echo $nbtSubExtractionElementID; ?>-<?php echo $subextraction['id']; ?>').fadeIn();">Delete</button>
-		<button class="nbtHidden" id="nbtSubRemoveExtraction<?php echo $nbtSubExtractionElementID; ?>-<?php echo $subextraction['id']; ?>" style="float: right;" onclick="nbtDeleteSubExtraction(<?php echo $nbtSubExtractionElementID; ?>, <?php echo $subextraction['id']; ?>);">For real</button>
-		<button style="float: right;" onclick="nbtMoveSubExtraction(<?php echo $nbtSubExtractionElementID; ?>, <?php echo $subextraction['id']; ?>, -1, <?php echo $nbtExtractRefSet; ?>, <?php echo $nbtExtractRefID; ?>, <?php echo $_SESSION[INSTALL_HASH . '_nbt_userid']; ?>);">&#8595;</button>
-		<button style="float: right;" onclick="nbtMoveSubExtraction(<?php echo $nbtSubExtractionElementID; ?>, <?php echo $subextraction['id']; ?>, 1, <?php echo $nbtExtractRefSet; ?>, <?php echo $nbtExtractRefID; ?>, <?php echo $_SESSION[INSTALL_HASH . '_nbt_userid']; ?>);">&#8593;</button>
-		<?php
+?><div class="nbtSubExtraction" id="nbtSubExtractionInstance<?php echo $nbtSubExtractionElementID; ?>-<?php echo $subextraction['id']; ?>">
+    <button style="float: right;" onclick="$(this).fadeOut(0);$('#nbtSubRemoveExtraction<?php echo $nbtSubExtractionElementID; ?>-<?php echo $subextraction['id']; ?>').fadeIn();">Delete</button>
+    <button class="nbtHidden" id="nbtSubRemoveExtraction<?php echo $nbtSubExtractionElementID; ?>-<?php echo $subextraction['id']; ?>" style="float: right;" onclick="nbtDeleteSubExtraction(<?php echo $nbtSubExtractionElementID; ?>, <?php echo $subextraction['id']; ?>);">For real</button>
+    <button style="float: right;" onclick="nbtMoveSubExtraction(<?php echo $nbtSubExtractionElementID; ?>, <?php echo $subextraction['id']; ?>, -1, <?php echo $nbtExtractRefSet; ?>, <?php echo $nbtExtractRefID; ?>, <?php echo $_SESSION[INSTALL_HASH . '_nbt_userid']; ?>);">&#8595;</button>
+    <button style="float: right;" onclick="nbtMoveSubExtraction(<?php echo $nbtSubExtractionElementID; ?>, <?php echo $subextraction['id']; ?>, 1, <?php echo $nbtExtractRefSet; ?>, <?php echo $nbtExtractRefID; ?>, <?php echo $_SESSION[INSTALL_HASH . '_nbt_userid']; ?>);">&#8593;</button>
+    <?php
 
-		foreach ( $subelements as $subelement ) {
+    foreach ( $subelements as $subelement ) {
 
-			switch ( $subelement['type'] ) {
+	switch ( $subelement['type'] ) {
 
-				case "open_text":
+	    case "open_text":
 
-					?><div id="nbtSubelementContainer<?php echo $subelement['id'] ?>-<?php echo $subextraction['id']; ?>"<?php if ($subelement['startup_visible'] != 1) { echo ' class="nbtHidden"'; } ?>><?php
+		echo '<div id="nbtSubelementContainer' . $subelement['id'] . '-' . $subextraction['id'] . '"';
 
-						nbt_echo_display_name_and_codebook ( $subelement['displayname'], $subelement['codebook'] );
+		if ($subelement['startup_visible'] != 1) {
+		    echo ' class="nbtHidden"';
+		}
 
-						nbt_echo_subextraction_text_field ($nbtSubExtractionElementID, $subextraction, $subelement['dbname'], 500, FALSE, $subelement['regex']); // Needs fixin'
+		echo '>';
 
-					if ( $previous != NULL & $subelement['copypreviousprompt'] == 1 ) {
+		nbt_echo_display_name_and_codebook ( $subelement['displayname'], $subelement['codebook'] );
 
-						?><button style="display: block; margin-top: 4px;" onclick="$('#nbtSub<?php echo $subextraction['id']; ?>TextField<?php echo $subelement['dbname']; ?>').val( $('#nbtSub<?php echo $previous['id']; ?>TextField<?php echo $subelement['dbname']; ?>').val() ); nbtSaveSubExtractionTextField(<?php echo $nbtSubExtractionElementID; ?>, <?php echo $subextraction['id']; ?>, '<?php echo $subelement['dbname']; ?>', 'nbtSub<?php echo $subextraction['id']; ?>TextField<?php echo $subelement['dbname']; ?>', 'nbtSub<?php echo $subextraction['id']; ?>TextField<?php echo $subelement['dbname']; ?>Feedback');">Copy from previous</button><?php
+		nbt_echo_subextraction_text_field ($nbtSubExtractionElementID, $subextraction, $subelement['dbname'], 500, FALSE, $subelement['regex']); // Needs fixin'
 
-					}
+		if ( $previous != NULL & $subelement['copypreviousprompt'] == 1 ) {
 
-					?></div><?php
+		    echo '<button style="display: block; margin-top: 4px;" onclick="$(\'#nbtSub' . $subextraction['id'] . 'TextField' . $subelement['dbname'] . '\').val( $(\'#nbtSub' . $previous['id'] . 'TextField' . $subelement['dbname'] . '\').val() ); nbtSaveSubExtractionTextField(' . $nbtSubExtractionElementID . ', ' . $subextraction['id'] . ', \'' . $subelement['dbname'] . '\', \'nbtSub' . $subextraction['id'] . 'TextField' . $subelement['dbname'] . '\', \'nbtSub' . $subextraction['id'] . 'TextField' . $subelement['dbname'] . 'Feedback\');">Copy from previous</button>';
 
-				break;
+		}
+
+		echo '</div>';
+
+		break;
+
+	    case "tags":
+
+		echo '<div id="nbtSubelementContainer' . $subelement['id'] . '-' . $subextraction['id'] . '"';
+
+		if ($subelement['startup_visible'] != 1) {
+		    echo ' class="nbtHidden"';
+		}
+
+		echo '>';
+
+		nbt_echo_display_name_and_codebook ( $subelement['displayname'], $subelement['codebook'] );
+
+		$tagprompts = explode(";", $subelement['tagprompts']);
+		$tagprompts = array_map('trim', $tagprompts);
+
+		echo '<p>Tag prompts</p>';
+
+		echo '<input type="hidden" id="nbtElementTagsPrompts' . $element['id'] . '" value="' . $element['tagprompts'] . '">';
+		
+		echo '<table class="nbtTabledData">';
+
+		echo '<tr class="nbtTableHeaders"><td colspan="2"><input class="TagSearch' . $subelement['id'] . '" type="text" onkeyup="nbtSearchTagsPrompts(' . $subelement['id'] . ');" placeholder="Search tag prompts"></td></tr>';
+		echo '<tr><td colspan="2"><button onclick="$(\'.TagPromptRow\').fadeIn(0);$(\'#TagSearch' . $element['id'] . '\').val(\'\');">Show all</button> <button onclick="$(\'.TagPromptRow\').fadeOut(0);$(\'#TagSearch' . $element['id'] . '\').val(\'\');">Show none</button></td></tr>';
+
+		foreach ($tagprompts as $tagprompt) {
+		    echo '<tr class="TagPromptRow TagPrompts' . $element['id'] . '">';
+
+		    echo '<td class="TagPromptCell">' . $tagprompt . '</td>';
+		    echo '<td style="text-align: right;"><button onclick="nbtAddTagToSelected(' . $element['id'] . ', $(this).parent().parent().find(\'.TagPromptCell\').html(), ' . $extraction['id'] . ', ' . $_GET['form'] . ', \'' . $element['columnname'] . '\');">Copy tag</button></td>';
+
+		    echo "</tr>";
+		}
+
+		echo "</table>";
+
+		
+
+		echo '</div>';
+		
+		break;
 
 				case "date_selector":
 
@@ -112,7 +159,7 @@ foreach ( $subextractions as $subextraction ) {
 					?></div><?php
 
 				break;
-
+						
 				case "table_data":
 
 					?><div id="nbtSubelementContainer<?php echo $subelement['id']; ?>-<?php echo $subextraction['id']; ?>"<?php if ($subelement['startup_visible'] != 1) { echo ' class="nbtHidden"'; } ?>><?php
