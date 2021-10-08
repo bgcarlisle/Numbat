48f4142db13ece555be58d65d198f46769cce19b
bgcarlisle <bgcarlisle@users.noreply.github.com>
2021-10-08 10:40:32 +0200

    Added tagprompts to installer db schema

--- install/writeconfig.php
+++ install/writeconfig.php
@@ -96,7 +96,7 @@
 	try { // form elements table
 
 	    $dbh = new PDO('mysql:dbname=' . $_POST['dbname'] . ';host=' . $_POST['dbhost'], $_POST['dbusername'], $_POST['dbpassword'], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-	    $stmt = $dbh->prepare("DROP TABLE IF EXISTS `formelements`; CREATE TABLE `formelements` ( `id` int(11) unsigned NOT NULL AUTO_INCREMENT, `formid` int(11) DEFAULT NULL, `sortorder` int(11) DEFAULT NULL, `type` varchar(20) DEFAULT NULL, `columnname` varchar(500) DEFAULT NULL, `displayname` varchar(200) DEFAULT NULL, `codebook` varchar(2000) DEFAULT NULL, `toggle` varchar(50) DEFAULT NULL, `startup_visible` INT(11) NULL DEFAULT 1, `conditional_logical_operator` CHAR(3) NULL DEFAULT 'any', `destructive_hiding` INT(11) NULL DEFAULT 1, `regex` varchar(500) DEFAULT NULL, PRIMARY KEY (`id`) ) ENGINE=MyISAM DEFAULT CHARSET=latin1;");
+	    $stmt = $dbh->prepare("DROP TABLE IF EXISTS `formelements`; CREATE TABLE `formelements` ( `id` int(11) unsigned NOT NULL AUTO_INCREMENT, `formid` int(11) DEFAULT NULL, `sortorder` int(11) DEFAULT NULL, `type` varchar(20) DEFAULT NULL, `columnname` varchar(500) DEFAULT NULL, `displayname` varchar(200) DEFAULT NULL, `codebook` varchar(2000) DEFAULT NULL, `toggle` varchar(50) DEFAULT NULL, `startup_visible` INT(11) NULL DEFAULT 1, `conditional_logical_operator` CHAR(3) NULL DEFAULT 'any', `destructive_hiding` INT(11) NULL DEFAULT 1, `regex` varchar(500) DEFAULT NULL, `tagprompts` TEXT DEFAULT NULL, PRIMARY KEY (`id`) ) ENGINE=MyISAM DEFAULT CHARSET=latin1;");
 
 	    if ( $stmt->execute() ) {
 
@@ -231,7 +231,7 @@
 	try { // sub elements table
 
 	    $dbh = new PDO('mysql:dbname=' . $_POST['dbname'] . ';host=' . $_POST['dbhost'], $_POST['dbusername'], $_POST['dbpassword'], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-	    $stmt = $dbh->prepare("DROP TABLE IF EXISTS `subelements`; CREATE TABLE `subelements` ( `id` int(11) unsigned NOT NULL AUTO_INCREMENT, `elementid` int(11) DEFAULT NULL, `sortorder` int(11) DEFAULT NULL, `type` varchar(20) DEFAULT NULL, `dbname` varchar(50) DEFAULT NULL, `displayname` varchar(200) DEFAULT NULL, `codebook` varchar(2000) DEFAULT NULL, `toggle` varchar(50) DEFAULT NULL, `startup_visible` INT(11) NULL DEFAULT 1, `conditional_logical_operator` CHAR(3) NULL DEFAULT 'any', `destructive_hiding` INT(11) NULL DEFAULT 1, `regex` varchar(500) DEFAULT NULL, PRIMARY KEY (`id`) ) ENGINE=MyISAM DEFAULT CHARSET=latin1;");
+	    $stmt = $dbh->prepare("DROP TABLE IF EXISTS `subelements`; CREATE TABLE `subelements` ( `id` int(11) unsigned NOT NULL AUTO_INCREMENT, `elementid` int(11) DEFAULT NULL, `sortorder` int(11) DEFAULT NULL, `type` varchar(20) DEFAULT NULL, `dbname` varchar(50) DEFAULT NULL, `displayname` varchar(200) DEFAULT NULL, `codebook` varchar(2000) DEFAULT NULL, `toggle` varchar(50) DEFAULT NULL, `startup_visible` INT(11) NULL DEFAULT 1, `conditional_logical_operator` CHAR(3) NULL DEFAULT 'any', `destructive_hiding` INT(11) NULL DEFAULT 1, `regex` varchar(500) DEFAULT NULL, `tagprompts` TEXT DEFAULT NULL, PRIMARY KEY (`id`) ) ENGINE=MyISAM DEFAULT CHARSET=latin1;");
 
 	    if ( $stmt->execute() ) {
 
